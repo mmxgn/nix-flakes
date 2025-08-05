121d2a2e5aa93346eba47d56a1dc54928cbcdb40
mmxgn <>
2025-08-05 12:20:31 +0200

    Add python311-fhs

---
+++ python311-fhs/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A very basic python 3.11 flake in an FHS environment which works for development with pytorch";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+  };
+
+  inputs.flake-utils.inputs.system.follows = "nixpkgs";
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {system = "${system}";};
+        simplePythonEnv = pkgs.python311.withPackages (ps: with ps; [pip virtualenv]);
+      in {
+        devShell =
+          (
+            pkgs.buildFHSEnv
+            {
+              name = "python-311-fhs env";
+              targetPkgs = p: [simplePythonEnv p.libGL p.glib];
+            }
+          )
+          .env;
+      }
+    );
+}
